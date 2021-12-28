720da6995851dfe24259435d5b6549a8c0fe4041
meebox <meebox@gmail.com>
2021-12-28 14:20:32 +0800

    修正 5 向按鈕電阻梯 ADC 值判斷及刪除未用到的 RTTTL 類別
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   FlagArcade.mpy
    	modified:   lib/FlagArcade.py
    
        - 5 向按鈕電阻梯 ADC 值改用分壓減 1/3 分壓區間當緩衝判斷條件
    
          原始分壓  調整後
          ADC 值   緩衝
             0
                    97-----------
           146          RST
                   243-----------
           292          SET
                   390-----------
           438          MID
                   536-----------
           585          RHT
                   682-----------
           731          LFT
                   828-----------
           877          DOWN
                   975-----------
          1024          UP
    
        - 刪除根本沒用到的 RTTTL 類別

--- lib/FlagArcade.py
+++ lib/FlagArcade.py
@@ -22,21 +22,19 @@ def randint(start, stop):
 
 def getKey(value):
     key='n'
-    if value<80 :
-        key='n'
-    elif abs(value-1024)<50:
+    if value > 975:
         key='u'
-    elif abs(value-950)<50:
+    elif value > 828:
         key='d'
-    elif abs(value-794)<50:
+    elif value > 682:
         key='l'
-    elif abs(value-633)<50:
+    elif value > 536:
         key='r'
-    elif abs(value-476)<50:
+    elif value > 390:
         key='m'
-    elif abs(value-318)<50:
+    elif value > 243:
         key='set'
-    elif abs(value-162)<50:
+    elif value > 97:
         key='rst'
     return key
 
@@ -293,164 +291,3 @@ class Character:
                 self.pixels_idx = 0
 
         #print('------------------------') #########
-
-
-
-NOTE = [
-    440.0,  # A
-    493.9,  # B or H
-    261.6,  # C
-    293.7,  # D
-    329.6,  # E
-    349.2,  # F
-    392.0,  # G
-    0.0,    # pad
-
-    466.2,  # A#
-    0.0,
-    277.2,  # C#
-    311.1,  # D#
-    0.0,
-    370.0,  # F#
-    415.3,  # G#
-    0.0,
-]
-
-
-# You can find a description of RTTTL here:
-# https://en.wikipedia.org/wiki/Ring_Tone_Transfer_Language
-class RTTTL:
-    def __init__(self, buzzer, defaults, tune):
-        self.buzzer = buzzer
-        self.tune = tune
-        self.tune_idx = 0
-        self.parse_defaults(defaults)
-        self.repeat_play = False
-        self.repeat_play_startidx = 0
-
-    def parse_defaults(self, defaults):
-        # Example: d=4,o=5,b=140
-        val = 0
-        id = ' '
-        for char in defaults:
-            char = char.lower()
-            if char.isdigit():
-                val *= 10
-                val += ord(char) - ord('0')
-                if id == 'o':
-                    self.default_octave = val
-                elif id == 'd':
-                    self.default_duration = val
-                elif id == 'b':
-                    self.bpm = val
-            elif char.isalpha():
-                id = char
-                val = 0
-        # 240000 = 60 sec/min * 4 beats/whole-note * 1000 msec/sec
-        self.msec_per_whole_note = 240000.0 / self.bpm
-
-    def play_background(self, timer):
-        if self.tune_idx >= 0:
-            freq, msec = next(self.notes())
-        else:
-            freq, msec = 0, 50
-            
-        if freq > 0:
-            self.buzzer.freq(int(freq))
-            self.buzzer.duty(50)
-        
-        self.play_background_delay_time = int(msec * 0.1)
-        timer.init(period=int(msec * 0.9), mode=timer.ONE_SHOT,
-                 callback=self.play_background_delay)
-
-    def play_background_delay(self, timer):
-        self.buzzer.duty(0)
-        timer.init(period=self.play_background_delay_time, mode=timer.ONE_SHOT,
-            callback=self.play_background)
-
-    def play_background_restart(self):
-        self.tune_idx = 0
-
-    def play_background_stop(self):
-        self.tune_idx = -1
-
-    def next_char(self):
-        if self.tune_idx < len(self.tune):
-            char = chr(self.tune[self.tune_idx])
-            self.tune_idx += 1
-            if char == ',':
-                char = ' '
-            return char
-        return '|'
-
-    def notes(self):
-        """Generator which generates notes. Each note is a tuple where the
-           first element is the frequency (in Hz) and the second element is
-           the duration (in milliseconds).
-        """
-        while True:
-            # Skip blank characters and commas
-            char = self.next_char()
-            while char == ' ':
-                char = self.next_char()
-
-            # Parse duration, if present. A duration of 1 means a whole note.
-            # A duration of 8 means 1/8 note.
-            duration = 0
-            while char.isdigit():
-                duration *= 10
-                duration += ord(char) - ord('0')
-                char = self.next_char()
-            if duration == 0:
-                duration = self.default_duration
-
-            if char == '|':  # marker for end of tune
-                if self.repeat_play:
-                    if self.repeat_play_startidx:
-                        self.tune_idx = self.repeat_play_startidx
-                    else:
-                        self.tune_idx = 0
-                    yield 0, 50
-                else:
-                    return
-
-            note = char.lower()
-            if 'a' <= note <= 'g':
-                note_idx = ord(note) - ord('a')
-            elif note == 'h':
-                note_idx = 1    # H is equivalent to B
-            else:
-                note_idx = 7    # pause
-            char = self.next_char()
-
-            # Check for sharp note
-            if char == '#':
-                note_idx += 8
-                char = self.next_char()
-
-            # Check for duration modifier before octave
-            # The spec has the dot after the octave, but some places do it
-            # the other way around.
-            duration_multiplier = 1.0
-            if char == '.':
-                duration_multiplier = 1.5
-                char = self.next_char()
-
-            # Check for octave
-            if '4' <= char <= '7':
-                octave = ord(char) - ord('0')
-                char = self.next_char()
-            else:
-                octave = self.default_octave
-
-            # Check for duration modifier after octave
-            if char == '.':
-                duration_multiplier = 1.5
-                char = self.next_char()
-
-            freq = NOTE[note_idx] * (1 << (octave - 4))
-            msec = (self.msec_per_whole_note / duration) * duration_multiplier
-
-            # print('note ', note, 'duration', duration, 'octave', octave, 'freq', freq, 'msec', msec)
-
-            yield freq, msec
